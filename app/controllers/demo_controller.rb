971c85184955d6f2ac785a7d4b9242a2f81302de
June <kimjune01@gmail.com>
2016-09-17 19:16:09 -0700

    add patients and treatments page

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,9 +1,24 @@
 class DemoController < ApplicationController
 
-
 def go
   puts "going!"
   render "demos/home"
+end
+
+def find_treatment
+	@treatments = Treatment.all
+  render "demos/treatments"
+end
+
+def find_patient
+	@patients = Patient.all
+  render 'demos/patients'
+end
+
+def new_doctor
+  @doctor = Doctor.new
+  render 'demos/new_doctor'
+  @doctor.save!
 end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,8 @@ Rails.application.routes.draw do
   resources :hospitalrails
 
   get '/demo', to: 'demo#go'
+  get '/find_patient', to: 'demo#find_patient'
+  get '/find_treatment', to: 'demo#find_treatment'
+  get '/new_doctor', to: 'demo#new_doctor'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
